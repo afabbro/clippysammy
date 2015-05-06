44c4f8bb0d3294f5336e287035695cfb77d70b7f
Ryan Quinn <ryan@digitalocean.com>
2015-05-06 12:34:43 -0400

    added droplet name field

--- bookmarklet.js
+++ bookmarklet.js
@@ -6,7 +6,10 @@ javascript:
 		event = event || window.event;
 		var element = event.target || event.srcElement;
 		alert(element.textContent);
-	}	
+	}
+        document.getElementById('appendedInput').onblur = function(event){
+                var dropName = document.getElementById('appendedInput').value;
+        }
 })()
 
  
