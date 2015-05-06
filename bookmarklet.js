72bf34c4e4841d3d35e7471340d57c82844d3e45
Angelina Fabbro <cyborg@digitalocean.com>
2015-05-06 12:32:17 -0400

    sample event handler to get started

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,12 @@
 javascript:
 (function(){
-	
-
-	
+	// example starter event handler
+	document.body.onclick = function(event) {
+		console.log("clicky");
+		event = event || window.event;
+		var element = event.target || event.srcElement;
+		alert(element.textContent);
+	}	
 })()
+
+ 
